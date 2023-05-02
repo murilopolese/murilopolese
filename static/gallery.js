f936abf0ac38f6140f51c81c9dadb980c4efb936
Murilo Polese <murilopolese@gmail.com>
2023-05-02 08:36:49 +0200

    Remove console.log

--- static/gallery.js
+++ static/gallery.js
@@ -1,6 +1,5 @@
 window.addEventListener('load', () => {
   let images = document.querySelectorAll('.gallery img')
-  console.log('images', images)
   if (images) {
     Array.from(images).forEach((image) => {
       image.addEventListener('click', (e) => {
